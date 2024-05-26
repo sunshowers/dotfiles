e353b2d916dd5f6eb7441e2606499a5aa8dfdb54
Rain <rain@sunshowers.io>
2024-05-26 00:17:05 -0700

    try out override_proxy_command

--- home/.wezterm.lua
+++ home/.wezterm.lua
@@ -6,6 +6,7 @@ local config = {
   font_size = 12.0,
   freetype_load_target = "HorizontalLcd",
   harfbuzz_features = {"ss01", "ss02", "ss03", "ss04", "ss06", "zero", "onum"},
+  initial_cols = 180,
   send_composed_key_when_left_alt_is_pressed = false,
   scrollback_lines = 5000,
   ssh_domains = {
@@ -13,10 +14,16 @@ local config = {
       name = "sunshowers-linux",
       remote_address = "sunshowers-linux.echoes",
       username = "rain",
-      remote_wezterm_path = "/home/rain/bin/wezterm-gui-mux",
+      override_proxy_command = "wezterm cli --no-auto-start proxy",
     },
   },
   ui_key_cap_rendering = "Emacs",
 };
+
+local hostname = wezterm.hostname()
+
+if hostname == 'cumulus' then
+  config.font_size = 9.0
+end
 
 return config;
